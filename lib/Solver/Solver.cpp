4c90df5463ae62ee961c124d5d416772aa1dfe9c
Dan Liew <daniel.liew@imperial.ac.uk>
2016-01-07 09:50:39 +0000

    [NFC] Refactor DummySolver out of Solver.cpp into its own file ``DummySolver.cpp``. Whilst I'm here also clang-format the modified code.

--- lib/Solver/Solver.cpp
+++ lib/Solver/Solver.cpp
@@ -278,45 +278,6 @@ std::pair< ref<Expr>, ref<Expr> > Solver::getRange(const Query& query) {
 
 /***/
 
-class DummySolverImpl : public SolverImpl {
-public: 
-  DummySolverImpl() {}
-  
-  bool computeValidity(const Query&, Solver::Validity &result) { 
-    ++stats::queries;
-    // FIXME: We should have stats::queriesFail;
-    return false; 
-  }
-  bool computeTruth(const Query&, bool &isValid) { 
-    ++stats::queries;
-    // FIXME: We should have stats::queriesFail;
-    return false; 
-  }
-  bool computeValue(const Query&, ref<Expr> &result, const Query& full_query) {
-    ++stats::queries;
-    ++stats::queryCounterexamples;
-    return false; 
-  }
-  bool computeInitialValues(const Query&,
-                            const std::vector<const Array*> &objects,
-                            std::vector< std::vector<unsigned char> > &values,
-                            bool &hasSolution) { 
-    ++stats::queries;
-    ++stats::queryCounterexamples;
-    return false; 
-  }
-  SolverRunStatus getOperationStatusCode() {
-      return SOLVER_RUN_STATUS_FAILURE;
-  }
-  
-};
-
-Solver *klee::createDummySolver() {
-  return new Solver(new DummySolverImpl());
-}
-
-/***/
-
 class STPSolverImpl : public SolverImpl {
 private:
   VC vc;
