1cc34b974c26d355c8acd0ec917c007ef7654115
Andrea Mattavelli <andreamattavelli@gmail.com>
2016-11-29 16:39:28 +0000

    Added  among the external calls that we model

--- tools/klee/main.cpp
+++ tools/klee/main.cpp
@@ -803,6 +803,7 @@ static const char *modelledExternals[] = {
   "klee_mark_global",
   "klee_merge",
   "klee_prefer_cex",
+  "klee_posix_prefer_cex",
   "klee_print_expr",
   "klee_print_range",
   "klee_report_error",
