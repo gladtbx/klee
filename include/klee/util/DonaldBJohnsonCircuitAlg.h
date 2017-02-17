b82b3428c1691a78d7be9a174223b6990f2ea0cd
gladtbx <gladtbx@gmail.com>
2017-02-17 11:03:50 -0600

    Work in progress

--- include/klee/util/DonaldBJohnsonCircuitAlg.h
+++ include/klee/util/DonaldBJohnsonCircuitAlg.h
@@ -4,12 +4,45 @@
 class dbjCircuit{
 private:
 	int n;//The number of nodes
-	std::vector<int> A;
+	std::vector<std::pair<int, errPercNode*> > A;
 	std::vector<int> B;
 	std::vector<bool> blocked;
+	std::stack<int> stack;
 	int s;
 	Graph<errPercNode>* graph;
+	errPercNode* root;
+	dbjCircuit():n(0),s(0),graph(NULL),root(NULL){
+
+	}
+
+	void clear(){
+		A.clear();
+		B.clear();
+		blocked.clear();
+		while(stack.size()){
+			stack.pop();
+		}
+		s = 0;
+	}
 public:
+	dbjCircuit(int _n, errPercNode* _root):n(_n),s(0),root(_root){
+		graph = new Graph<errPercNode>(n,root);
+		graph->init();
+	}
+
+	void genCircuit(){
+		clear();
+		s = 1;
+		while (s < n){
+			graph->SCC(s);
+			std::vector<std::set<std::pair<int,errPercNode*> > > partition = graph->getPartition();
+			for(std::vector<std::set<std::pair<int,errPercNode*> > >::iterator i = partition.begin(); i != partition.end(); i++){
+				for(std::set<std::pair<int,errPercNode*> >::iterator j = i->begin(); j != i->end(); j++ ){
+
+				}
+			}
+		}
+	}
 
 };
 
