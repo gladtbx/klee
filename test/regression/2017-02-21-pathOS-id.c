4c9f78299636ebc402b18ccb0bbb314ff07410dc
gladtbx <gladtbx@gmail.com>
2017-02-21 10:17:42 -0600

    Regression test added

--- test/regression/2017-02-21-pathOS-id.c
+++ test/regression/2017-02-21-pathOS-id.c
@@ -1,10 +1,11 @@
 // RUN: %llvmgcc %s -emit-llvm -g -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
 // RUN: %klee --output-dir=%t.klee-out -write-paths %t.bc 2> %t.log
-// RUN: cat %t.klee-out/test000001.path | wc -l | grep -q 1
-// RUN: cat %t.klee-out/test000002.path | wc -l | grep -q 1
-// RUN: cat %t.klee-out/test000003.path | wc -l | grep -q 1
-// RUN: cat %t.klee-out/test000004.path | wc -l | grep -q 1
+// RUN: cat %t.klee-out/test000001.path | wc -l | FileCheck %s
+// RUN: cat %t.klee-out/test000002.path | wc -l | FileCheck %s
+// RUN: cat %t.klee-out/test000003.path | wc -l | FileCheck %s
+// RUN: cat %t.klee-out/test000004.path | wc -l | FileCheck %s
+// CHECK: 1
 int main(){
 	int a, b;
 	klee_make_symbolic (&a, sizeof(int), "a");
