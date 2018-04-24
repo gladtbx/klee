038e32aadfb2c92bf78060c8c80f42c49577592a
Gladtbx <gladtbx@ecetwotwenty@gmail.com>
2018-04-23 19:20:32 -0500

    Executor.cpp updated

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3841,7 +3841,7 @@ void Executor::runFunctionAsMain(Function *f,
   if (statsTracker)
     statsTracker->framePushed(*state, 0);
 
-  //if(LoopReduction)
+  if(LoopReduction)
 	  processLoopInfo(&f->getEntryBlock());
   assert(arguments.size() == f->arg_size() && "wrong number of arguments");
   for (unsigned i = 0, e = f->arg_size(); i != e; ++i)
