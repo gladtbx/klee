ce07505572fe7c6fb237b79962b633466488ebbc
gladtbx <gladtbx@gmail.com>
2019-02-28 15:24:20 -0600

    cCache Solver turned on commit

--- lib/Solver/CCacheSolver.cpp
+++ lib/Solver/CCacheSolver.cpp
@@ -331,5 +331,6 @@ SolverImpl::SolverRunStatus CcacheSolver::getOperationStatusCode() {
 Solver *klee::createCCacheSolver(Solver *s){
 	//return new Solver(new CachingSolver(_solver));
 	//Need to implement a new layer here that contacts the outside cache.
-	return s;
+
+	return CcacheSolver(s);
 }
